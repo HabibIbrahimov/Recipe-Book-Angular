0e2c0a50ab71879e686bda0fbac6cc7bf5f5c251
HabibIbrahimov <habibyi@code.edu.az>
2022-05-17 14:46:38 +0400

    feat: Adding Navigation with Event Binding

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
